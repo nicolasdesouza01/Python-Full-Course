87fbc7105747a1f7dd1dddda8979bea647cf6139
Nicolas De Souza <nicolassouza6636@gmail.com>
2026-04-30 15:40:13 -0300

    Mundo-04(POO)

---
+++ Mundo-04POO/ex001.py
@@ -0,0 +1,26 @@
+# Declaração de Classe
+class Gafanhoto:
+    def __init__(self): #Método Construtor
+        # atributo de Instância
+        self.nome = ""
+        self.idade = 0        
+
+    # Método de instância
+    def aniversario(self):
+        self.idade = self.idade + 1
+
+    def mensagem (self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+
+# Declaração de Objetos
+g1 = Gafanhoto()
+g1.nome = "maria"
+g1.idade = 17
+g1.aniversario()
+print(g1.mensagem())
+
+g2 = Gafanhoto()
+g2.nome = "Mauro"
+g2.idade = 53
+g2.aniversario()
+print(g2.mensagem())
